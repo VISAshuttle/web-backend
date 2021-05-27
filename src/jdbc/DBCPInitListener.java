f552035b49a60386c5bbc20f0b2d2d74ae11be98
VISAshuttle <codemonkey@kakao.com>
2021-05-27 18:03:42 +0900

    DBCP Listener 방식으로 변경

---
+++ src/jdbc/DBCPInitListener.java
@@ -0,0 +1,92 @@
+package jdbc;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.dbcp2.ConnectionFactory;
+import org.apache.commons.dbcp2.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp2.PoolableConnection;
+import org.apache.commons.dbcp2.PoolableConnectionFactory;
+import org.apache.commons.dbcp2.PoolingDriver;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+public class DBCPInitListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		String poolConfig = sce.getServletContext().getInitParameter("poolConfig");
+		Properties prop = new Properties();
+		try {
+			prop.load(new StringReader(poolConfig));
+		} catch (IOException e) {
+			throw new RuntimeException("config load fail", e);
+		}
+		
+		loadJDBCDriver(prop.getProperty("jdbcDriver"));
+		
+		StringTokenizer stk = new StringTokenizer(prop.getProperty("dbName"), ", ");
+		while (stk.hasMoreTokens()) {
+			prop.setProperty("dbName", stk.nextToken());
+			initConnectionPool(prop);
+		}
+	}
+
+	private void loadJDBCDriver(String driverClass) {
+		try {
+			Class.forName(driverClass);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("fail to load JDBC Driver", e);
+		}
+	}
+	
+	private void initConnectionPool(Properties prop) {
+		try {
+			String jdbcUrl = 
+					prop.getProperty("jdbcUrl") +
+					prop.getProperty("dbName") +
+					"?characterEncoding=utf8";
+			String dbUser = prop.getProperty("dbUser");
+			String dbPass = prop.getProperty("dbPass");
+			ConnectionFactory connFactory = new DriverManagerConnectionFactory(jdbcUrl, dbUser, dbPass);
+
+			PoolableConnectionFactory poolableConnFactory = new PoolableConnectionFactory(connFactory, null);
+			String validationQuery = prop.getProperty("validationQuery");
+			if (validationQuery != null && !validationQuery.isEmpty()) {
+				poolableConnFactory.setValidationQuery(validationQuery);
+			}
+			GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+			poolConfig.setTimeBetweenEvictionRunsMillis(1000L * 60L * 5L);
+			poolConfig.setTestWhileIdle(true);
+			int minIdle = getIntProperty(prop, "minIdle", 5);
+			poolConfig.setMinIdle(minIdle);
+			int maxTotal = getIntProperty(prop, "maxTotal", 50);
+			poolConfig.setMaxTotal(maxTotal);
+
+			GenericObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(poolableConnFactory, poolConfig);
+			poolableConnFactory.setPool(connectionPool);
+			
+			Class.forName("org.apache.commons.dbcp2.PoolingDriver");
+			PoolingDriver driver = (PoolingDriver) DriverManager.getDriver("jdbc:apache:commons:dbcp:");
+			String poolName = prop.getProperty("poolName");
+			driver.registerPool(poolName, connectionPool);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private int getIntProperty(Properties prop, String propName, int defaultValue) {
+		String value = prop.getProperty(propName);
+		return (value == null) ? defaultValue : Integer.parseInt(value);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {}
+
+}
